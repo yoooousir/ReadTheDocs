789db1c3b36ab15972a0d1978d9c2655bbb22e49
yoooousir <gimchaeyeong53@gmail.com>
2024-11-30 22:53:27 +0900

    first commit

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'OSSW24_Team6'
+copyright = '2024, Chae-young KIM'
+author = 'Chae-young KIM'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+import sphinx_rtd_theme
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'recommonmark'
+]
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
---
+++ docs/source/contributing.rst
@@ -0,0 +1,27 @@
+.. OSSW24_Team6 documentation master file, created by
+   sphinx-quickstart on Sat Nov 30 22:45:54 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to OSSW24_Team6 documentation
+==========================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   about
+   getting_started
+   usage
+   technical_overview
+   api_reference
+   configuration
+   maintenance
+   contributing
+   faq
+   release_notes
